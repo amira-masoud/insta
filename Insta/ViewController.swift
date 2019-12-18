ad182effb2bde505d92cc6de046bf615689df4d0
Amira <amira@amiras-imac.local>
2019-12-18 05:09:15 -0800

    Initial Commit

---
+++ Insta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Insta
+//
+//  Created by Amira on 12/18/19.
+//  Copyright © 2019 Amira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
